48dcf36cb01788700c156469c3bb5e43748b1375
Oleg Menyaylenko <oleg.menyaylenko@gmail.com>
2019-11-09 17:16:37 -0500

    Added some docs

--- app/src/main/java/com/nutriscan/misc/listeners/OnModelClick.java
+++ app/src/main/java/com/nutriscan/misc/listeners/OnModelClick.java
@@ -2,6 +2,13 @@ package com.nutriscan.misc.listeners;
 
 import android.support.annotation.NonNull;
 
+/**
+ * Simple listener that has an onClick method that passes an object
+ * @param <E>
+ */
 public interface OnModelClick<E> {
+    /**
+     * @param model The "model" that was clicked
+     */
     void onClick(@NonNull E model);
 }
