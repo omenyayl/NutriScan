8dae087bbb2fa5e575afed17f034f2a9c2d4f263
Oleg Menyaylenko <oleg.menyaylenko@gmail.com>
2019-11-09 21:55:52 -0500

    Refactored

--- app/src/main/java/com/nutriscan/scan/ScanActivity.java
+++ app/src/main/java/com/nutriscan/scan/ScanActivity.java
@@ -8,10 +8,11 @@ import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+/**
+ * Launch this to scan a barcode, it will add the scanned text to its result intent data
+ */
 public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private static final String TAG = "ScanActivity";
     private ZXingScannerView mScannerView;
-
     public static final String INTENT_EXTRA_BARCODE = "barcode";
 
     @Override
@@ -34,16 +35,14 @@ public class ScanActivity extends AppCompatActivity implements ZXingScannerView.
         mScannerView.stopCamera();           // Stop camera on pause
     }
 
+    /**
+     * @param rawResult The barcode result
+     */
     @Override
     public void handleResult(Result rawResult) {
-        // Do something with the result here
-//        Log.v(TAG, rawResult.getText()); // Prints scan results
-//        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (QR code, pdf417 etc.)
         Intent finishIntent = new Intent();
         finishIntent.putExtra(INTENT_EXTRA_BARCODE, rawResult.toString());
         setResult(RESULT_OK, finishIntent);
         finish();
-        // If you would like to resume scanning, call this method below:
-//        mScannerView.resumeCameraPreview(this);
     }
 }
