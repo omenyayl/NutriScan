b8385baa436d21f915855f11d45baa58938e6066
maryama <malarcon@gmu.edu>
2019-11-15 18:04:03 -0500

    Added analysis activity view

---
+++ app/src/main/java/com/nutriscan/analysis/AnalysisActivity.java
@@ -0,0 +1,18 @@
+
+
+package com.nutriscan.analysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nutriscan.R;
+
+public class AnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+    }
+}
