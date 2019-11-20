1dc1dd650eda71555618c9d9f468cf908c91b4c7
maryama <malarcon@gmu.edu>
2019-11-19 20:52:47 -0500

    Added analysis activity view

--- app/src/main/java/com/nutriscan/analysis/AnalysisActivity.java
+++ app/src/main/java/com/nutriscan/analysis/AnalysisActivity.java
@@ -3,16 +3,27 @@
 package com.nutriscan.analysis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.nutriscan.R;
 
 public class AnalysisActivity extends AppCompatActivity {
+    private TextView textViewProductName;
+    private TextView textViewAnalysis;
+    private RecyclerView recyclerViewHealthFactors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
+    }
+
+    private void bindViews(){
+        this.textViewProductName = findViewById(R.id.textViewProductName);
+        this.textViewAnalysis = findViewById(R.id.textViewAnalysis);
+        this.recyclerViewHealthFactors = findViewById(R.id.recyclerViewHealthFactors);
     }
 }
