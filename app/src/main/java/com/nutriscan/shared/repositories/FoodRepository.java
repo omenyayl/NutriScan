c05e4a4b5093d131c24b1a67a94d8791e9a4e220
maryama <malarcon@gmu.edu>
2019-11-21 21:40:32 -0500

    working with food repository

--- app/src/main/java/com/nutriscan/shared/repositories/FoodRepository.java
+++ app/src/main/java/com/nutriscan/shared/repositories/FoodRepository.java
@@ -1,13 +1,24 @@
 package com.nutriscan.shared.repositories;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.nutriscan.misc.enums.NutrientType;
+import com.nutriscan.misc.enums.Unit;
+import com.nutriscan.shared.domain.Nutrient;
+import com.nutriscan.shared.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Repository for food
  */
 public class FoodRepository {
     private static FoodRepository instance;
+    private MutableLiveData <Product> scannedItem;
 
     private FoodRepository() {
-
+        this.scannedItem = new MutableLiveData<>();
     }
 
     public FoodRepository getInstance() {
@@ -15,6 +26,13 @@ public class FoodRepository {
         return instance;
     }
 
+    public MutableLiveData<Product> getScannedItem() {
+        return scannedItem;
+    }
 
-
+    private void mockScannedItem() {
+        List<Nutrient> nutrients = new ArrayList<>();
+//        nutrients.add(new Nutrient(NutrientType.ENERGY, 400, Unit.kcal));
+//        Product p = new Product();
+    }
 }
